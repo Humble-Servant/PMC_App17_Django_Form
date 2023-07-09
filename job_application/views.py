e4b5b8d6694529693ebeda800fb83d9cfd2306cf
We the People <69933074+Humble-Servant@users.noreply.github.com>
2023-07-09 15:05:04 -0400

    Successfully connected to gmail and sent confirmation email

--- job_application/views.py
+++ job_application/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .forms import ApplicationForm
 from .models import Form
 from django.contrib import messages
+from django.core.mail import EmailMessage
 
 
 def index(request):
@@ -15,5 +16,9 @@ def index(request):
             occupation = form.cleaned_data["occupation"]
             Form.objects.create(first_name=first_name, last_name=last_name, email=email,
                                 date_avail=date_avail, occupation=occupation)
+            message_body = f"A new job application was submitted.  \n\nThank you, {first_name} for your submission."
+            email_message = EmailMessage("Form Submission Confirmation", message_body, to=[email])
+            email_message.send()
+            
             messages.success(request, "Form submitted successfully!")
     return render(request, "index.html")
